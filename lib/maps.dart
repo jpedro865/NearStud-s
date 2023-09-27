4899fc07425f66fb8f61d503272979a27b635e88
caloré <fausta_b@etna-alternance.net>
2023-09-27 17:20:52 +0200

    ajout d'une appbar dynamique

--- lib/maps.dart
+++ lib/maps.dart
@@ -8,12 +8,55 @@ class MapsPage extends StatefulWidget {
 }
 
 class _MapsPageState extends State<MapsPage> {
+  String GoogleApiKey = "";
+  int _currentIndex = 0;
+
+  List<IconData> _icons = [
+    Icons.home,
+    Icons.favorite,
+    Icons.search,
+    Icons.maps_home_work,
+  ];
+
+  List<Color> _iconColors = [
+    Colors.blue,
+    Colors.blue,
+    Colors.blue,
+    Colors.blue,
+  ];
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    String GoogleApiKey ="";
-
     return Scaffold(
-      appBar: AppBar(backgroundColor: Color.fromARGB(73, 18, 230, 11),),
+      appBar: AppBar(
+        title: Text('Appbar'),
+      ),
+      body: Center(
+        child: Text('==> ${_currentIndex + 1}'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: _onTabTapped,
+        items: _icons.asMap().entries.map((entry) {
+          int index = entry.key;
+          IconData icon = entry.value;
+          Color color = _iconColors[index];
+
+          return BottomNavigationBarItem(
+            icon: Icon(
+              icon,
+              color: color,
+            ),
+            label: 'Item $index',
+          );
+        }).toList(),
+      ),
     );
   }
 }
