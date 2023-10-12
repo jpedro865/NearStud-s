b95723ca0458f093cd4dc89074c940bb4c4ccab9
caloré <fausta_b@etna-alternance.net>
2023-10-12 20:28:25 +0200

    28 septembre

---
+++ lib/widgets/productPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ProductPage(),
+    );
+  }
+}
