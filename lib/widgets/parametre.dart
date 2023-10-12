b34605a8f18becb14b02467f94f0d7de879ad151
caloré <fausta_b@etna-alternance.net>
2023-10-12 20:26:09 +0200

    .

--- lib/widgets/parametre.dart
+++ lib/widgets/parametre.dart
@@ -18,38 +18,38 @@ class SettingsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Paramètres'),
+        title: Text('Parametres'),
       ),
       body: ListView(
         children: <Widget>[
           ListTile(
             title: Text('Mon compte'),
             onTap: () {
-              // Action lorsque "Mon compte" est cliqué
+             
             },
           ),
   ListTile(
             title: Text('Confidentialité'),
             onTap: () {
-              // Action lorsque "Confidentialité" est cliqué
+              
             },
           ),
           ListTile(
             title: Text('Données personnelles'),
             onTap: () {
-              // Action lorsque "Données personnelles" est cliqué
+              
             },
           ),
           ListTile(
             title: Text('Abonnement'),
             onTap: () {
-              // Action lorsque "Abonnement" est cliqué
+              
             },
           ),
           ListTile(
             title: Text('Se déconnecter'),
             onTap: () {
-              // Action lorsque "Se déconnecter" est cliqué
+              
             },
           ),
         ],
